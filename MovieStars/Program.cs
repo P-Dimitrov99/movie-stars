e62d14cc0ef3592b664bb8c890bc0742b2e1f563
P-Dimitrov99 <90553649+P-Dimitrov99@users.noreply.github.com>
2022-06-28 13:25:11 +0200

    Task 1 - Movie stars
    Sorry for uploading only the files without sorting them in folders

---
+++ MovieStars/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace MovieStars
+{
+    class Program
+    {
+        public class MovieStar
+        {
+            public DateTime dateOfBirth { get; set; }
+            public string Name { get; set; }
+            public string Surname { get; set; }
+            public string Sex { get; set; }
+            public string Nationality { get; set; }
+            public int age { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            List<MovieStar> movieStars = new List<MovieStar>();
+            string filePath = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"C:\Users\admin 1\Documents\Visual Studio 2017\Projects\MovieStars")) + @"\input.txt";
+            string movieStarJson = File.ReadAllText(filePath);
+            movieStars = (JsonConvert.DeserializeObject<List<MovieStar>>(movieStarJson));
+
+            foreach (MovieStar star in movieStars)
+            {
+                int totalDays = DateTime.Now.Subtract(star.dateOfBirth).Days;
+                star.age = totalDays / 365;
+
+                Console.WriteLine("{0} {1}", star.Name, star.Surname);
+                Console.WriteLine(star.Sex);
+                Console.WriteLine(star.Nationality);
+                Console.WriteLine(star.age);
+                Console.WriteLine(' ');
+                
+            }
+
+        }
+        
+    }
+}
